54f3e90544a2b84f2ec39ea25762962c69cb2497
emirdeliz <emir.marques@condoconta.com>
2022-08-23 21:28:06 +0000

    feat: Work in progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:menu_float/menu_float.dart';
 import 'package:menu_float_demo/__mock__/menu_float.mock.dart';
+import 'package:menu_float_demo/menu_float_app.dart';
 
 void main() {
   runApp(const MenuFloatDemo());
@@ -27,55 +28,55 @@ class MenuFloatDemo extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MenuFloatDemoPage(title: 'Menu Float Demo Page'),
+      home: const MenuFloatAppTestPage(target: ),
     );
   }
 }
 
-class MenuFloatDemoPage extends StatefulWidget {
-  const MenuFloatDemoPage({Key? key, required this.title}) : super(key: key);
-  final String title;
+// class MenuFloatDemoPage extends StatefulWidget {
+//   const MenuFloatDemoPage({Key? key, required this.title}) : super(key: key);
+//   final String title;
 
-  @override
-  State<MenuFloatDemoPage> createState() => _MenuFloatDemoPageState();
-}
+//   @override
+//   State<MenuFloatDemoPage> createState() => _MenuFloatDemoPageState();
+// }
 
-class _MenuFloatDemoPageState extends State<MenuFloatDemoPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: null,
-        body: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                MenuFloat<Product>(
-                  title: 'Hello menu float',
-                  items: menusOptions,
-                  child: const Text('Click me'),
-                ),
-                MenuFloat<Product>(
-                  title: 'Hello menu float top',
-                  top: true,
-                  items: menusOptions,
-                  child: const Text('Click me'),
-                ),
-                MenuFloat<Product>(
-                  title: 'Hello menu float Left',
-                  left: true,
-                  items: menusOptions,
-                  child: const Text('Click me'),
-                ),
-                MenuFloat(
-                  title: 'Hello menu float Right',
-                  right: true,
-                  items: menusOptions,
-                  child: const Text('Click me'),
-                )
-              ],
-            )));
-  }
-}
+// class _MenuFloatDemoPageState extends State<MenuFloatDemoPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: null,
+//         body: Container(
+//             alignment: Alignment.center,
+//             child: Column(
+//               mainAxisSize: MainAxisSize.max,
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 MenuFloat<Product>(
+//                   title: 'Hello menu float',
+//                   items: menusOptions,
+//                   child: const Text('Click me'),
+//                 ),
+//                 MenuFloat<Product>(
+//                   title: 'Hello menu float top',
+//                   top: true,
+//                   items: menusOptions,
+//                   child: const Text('Click me'),
+//                 ),
+//                 MenuFloat<Product>(
+//                   title: 'Hello menu float Left',
+//                   left: true,
+//                   items: menusOptions,
+//                   child: const Text('Click me'),
+//                 ),
+//                 MenuFloat(
+//                   title: 'Hello menu float Right',
+//                   right: true,
+//                   items: menusOptions,
+//                   child: const Text('Click me'),
+//                 )
+//               ],
+//             )));
+//   }
+// }
---
+++ lib/menu_float_app.dart
@@ -0,0 +1,99 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:menu_float/menu_float.dart';
+
+import '__mock__/menu_float.mock.dart';
+
+final menusOptions = productMock.map<MenuFloatOption<Product>>((e) {
+  return MenuFloatOption<Product>(
+      label: e.name,
+      value: e,
+      onClick: (Product v) {
+        print(v.name);
+      });
+}).toList();
+
+class MenuFloatAppTest extends StatelessWidget {
+  final Widget target;
+  final bool top;
+  final bool left;
+  final bool right;
+  final double x;
+  final double y;
+
+  const MenuFloatAppTest({
+    Key? key,
+    required this.target,
+    this.top = false,
+    this.left = false,
+    this.right = false,
+    required this.x,
+    required this.y,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MenuFloatAppTestPage(
+          target: target, x: x, y: y, top: top, left: left, right: right),
+    );
+  }
+}
+
+class MenuFloatAppTestPage extends StatefulWidget {
+  final bool top;
+  final bool left;
+  final bool right;
+  final double x;
+  final double y;
+
+  const MenuFloatAppTestPage(
+      {Key? key,
+      required this.target,
+      required this.x,
+      required this.y,
+      this.top = false,
+      this.left = false,
+      this.right = false})
+      : super(key: key);
+  final Widget target;
+
+  @override
+  State<MenuFloatAppTestPage> createState() => _MenuFloatAppTestPageState();
+}
+
+class _MenuFloatAppTestPageState extends State<MenuFloatAppTestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          left: widget.x,
+          top: widget.y,
+          child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MenuFloat<Product>(
+                    title: 'Hello menu float',
+                    items: menusOptions,
+                    top: widget.top,
+                    left: widget.left,
+                    right: widget.right,
+                    child: widget.target,
+                  )
+                ],
+              )),
+        ),
+      ],
+    );
+  }
+}
